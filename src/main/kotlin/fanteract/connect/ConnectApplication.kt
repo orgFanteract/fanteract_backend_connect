7b30deccc13b058beccf2649f0c67faabbe7a68e
추호성 <hosung@chuhoseong-ui-MacBookPro.local>
2025-12-05 21:49:05 +0900

    INIT

---
+++ src/main/kotlin/fanteract/connect/ConnectApplication.kt
@@ -0,0 +1,11 @@
+package fanteract.connect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConnectApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConnectApplication>(*args)
+}
---
+++ src/test/kotlin/fanteract/connect/ConnectApplicationTests.kt
@@ -0,0 +1,13 @@
+package fanteract.connect
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConnectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
