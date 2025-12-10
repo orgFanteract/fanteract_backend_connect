a071f1a5a5b88cea3dded7cb3c59735d4572860f
Ho Sung <57245883+HoSungChoo@users.noreply.github.com>
2025-12-10 10:55:15 +0900

    서킷브레이커 구축 및 CI/CD 개선 (#11)
    서킷브레이커 구축 및 CI/CD 개선 (#11)

--- src/main/kotlin/fanteract/connect/client/UserClient.kt
+++ src/main/kotlin/fanteract/connect/client/UserClient.kt
@@ -6,6 +6,12 @@ import fanteract.connect.dto.client.ReadUserListInnerResponse
 import fanteract.connect.dto.client.UpdateAbusePointInnerRequest
 import fanteract.connect.dto.client.UpdateActivePointInnerRequest
 import fanteract.connect.dto.client.UpdateBalanceInnerRequest
+import fanteract.connect.exception.ExceptionType
+import fanteract.connect.exception.MessageType
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
+import io.github.resilience4j.retry.annotation.Retry
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClient
@@ -14,16 +20,14 @@ import kotlin.jvm.java
 
 @Component
 class UserClient(
-    @Value("\${client.account-service.url}") userServiceUrl: String,
+    @Value($$"${client.account-service.url}") userServiceUrl: String,
     private val restClient: RestClient = RestClient.builder()
         .baseUrl(userServiceUrl)
         .build(),
+    private val circuitBreakerRegistry: CircuitBreakerRegistry
 ) {
-
-    /**
-     * 유저 존재 여부 확인
-     * GET /internal/users/{userId}/exists
-     */
+    @CircuitBreaker(name = "user-service", fallbackMethod = "getExistsByIdFallback")
+    @Retry(name = "user-service")
     fun existsById(userId: Long): Boolean {
         val response = restClient.get()
             .uri("/internal/users/{userId}/exists", userId)
@@ -33,10 +37,15 @@ class UserClient(
         return response?.exists ?: false
     }
 
-    /**
-     * 단건 유저 조회
-     * GET /internal/users/{userId}
-     */
+    fun getExistsByIdFallback(userId: Long, ex: Throwable): Boolean {
+        val cb = circuitBreakerRegistry.circuitBreaker("user-service")
+        val state = cb.state
+
+        println("existsByIdFallback userId=$userId, ex=$ex, state=$state")
+        throw ExceptionType.withType(MessageType.INVALID_ACTION)
+    }
+    
+    @Retry(name = "user-service")
     fun findById(userId: Long): ReadUserInnerResponse {
         val response = restClient.get()
             .uri("/internal/users/{userId}", userId)
@@ -46,10 +55,8 @@ class UserClient(
         return requireNotNull(response) { "User not found for id=$userId" }
     }
 
-    /**
-     * 유저 잔액 수정
-     * PUT /internal/users/{userId}/balance
-     */
+    @CircuitBreaker(name = "user-service", fallbackMethod = "getUpdateBalanceFallback")
+    @Retry(name = "user-service")
     fun updateBalance(userId: Long, balance: Int) {
         val request = UpdateBalanceInnerRequest(balance = balance)
 
@@ -60,10 +67,16 @@ class UserClient(
             .toBodilessEntity()
     }
 
-    /**
-     * 유저 사용 가능 포인트 수정
-     * PUT /internal/users/{userId}/active-point
-     */
+    fun getUpdateBalanceFallback(userId: Long, balance: Int, ex: Throwable) {
+        val cb = circuitBreakerRegistry.circuitBreaker("user-service")
+        val state = cb.state
+
+        println("existsByIdFallback userId=$userId, ex=$ex, state=$state")
+        throw ExceptionType.withType(MessageType.INVALID_ACTION)
+    }
+
+    @CircuitBreaker(name = "user-service", fallbackMethod = "getUpdateActivePointFallback")
+    @Retry(name = "user-service")
     fun updateActivePoint(userId: Long, activePoint: Int) {
         val request = UpdateActivePointInnerRequest(activePoint = activePoint)
 
@@ -74,10 +87,15 @@ class UserClient(
             .toBodilessEntity()
     }
 
-    /**
-     * 여러 유저 ID 조회
-     * GET /internal/users/batch?userIds=1&userIds=2...
-     */
+    fun getUpdateActivePointFallback(userId: Long, balance: Int, ex: Throwable) {
+        val cb = circuitBreakerRegistry.circuitBreaker("user-service")
+        val state = cb.state
+
+        println("existsByIdFallback userId=$userId, ex=$ex, state=$state")
+        throw ExceptionType.withType(MessageType.INVALID_ACTION)
+    }
+
+    @Retry(name = "user-service")
     fun findByIdIn(userIds: List<Long>): List<ReadUserInnerResponse> {
         val response = restClient.get()
             .uri { builder ->
@@ -92,10 +110,7 @@ class UserClient(
         return response?.users ?: emptyList()
     }
 
-    /**
-     * 유저 부정 포인트 수정
-     * PUT /internal/users/{userId}/abuse-point
-     */
+    @Retry(name = "user-service")
     fun updateAbusePoint(userId: Long, abusePoint: Int) {
         val request = UpdateAbusePointInnerRequest(abusePoint = abusePoint)
 
